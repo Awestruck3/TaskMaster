916bc770d13f1e21f4eb93334998fee34e86bd84
scot2950 <scot2950@ISS2V1SKH2.scdsb.pri>
2017-12-15 14:02:38 -0500

    Updated TaskMaster

--- src/Task.java
+++ src/Task.java
@@ -1,4 +1,21 @@
 
 public class Task {
+    private String name;
+    private String description;
     
+    public Task(String name, String description){
+        name="";
+        description="";
+    }
+    
+    public boolean validate(){
+        if(name==null||description==null||name.equals("")||description.equals("")){
+            return false;
+        }
+        else
+            return false;
+        }
+    public String toString(){
+        return "Name: " + name + "\nDescription: " + description + "\n=======\n";
+    }
 }
